4655b8c43f08c3aef0e510a44e75c1a8bca46357
Aman Das <amandas.das@gmail.com>
2021-08-24 07:11:34 +0530

    main

--- Return all Orphaned OneDrive sites.ps1
+++ Return all Orphaned OneDrive sites.ps1
@@ -1,17 +1,27 @@
 
+<#...
+Prerequisites
+#Install AAD module and SPO Module
+Install-Module AzureAD  -Force
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force
+# 
+...#>
 
-#Make sure to connect to AAD  and SPOservice first.
-#Get all ODB sites
+Connect-AzureAD 
+Connect-SPOService 
+
+# Get all ODB sites
 $ODBSites = Get-SPOSite -Template "SPSPERS" -Limit ALL -IncludePersonalSite $True
+
 # Get all AzureAD Accounts and create hash table for lookup
 $AADUsers = Get-AzureADUser -All $True -Filter "Usertype eq 'Member'" | Select UserPrincipalName, DisplayName
 $AADAccounts = @{} 
-$AADUsers.ForEach( {
-            $AADAccounts.Add([String]$_.UserPrincipalName, $_.DisplayName) } )
+$AADUsers.ForEach( { $AADAccounts.Add([String]$_.UserPrincipalName, $_.DisplayName) } )
+
 # Find the Orphaned sites by comparing the Site Owners to AAD
 ForEach ($Site in $ODBSites) {
       If (!($AADAccounts.Item($Site.Owner))) {
-            #Allocate a new owner to the OneDrive site
+            # Report
             Write-Host "Orphaned ODB sites " $Site.URL
       }
 }
