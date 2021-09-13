5094e67fe7f0e90b5d4c18ec5efe6a1321cd3426
Aman Das <amandas.das@gmail.com>
2021-09-13 06:18:28 +0530

    initialcommit

---
+++ Update Hub site logo to its assosiated sites.ps1
@@ -0,0 +1,44 @@
+$tenantAdmin = Read-Host "Enter SPO admin URL "
+$hubSiteurl = Read-Host "Enter HUb Site URL " 
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+
+}
+$null = $FileBrowser.ShowDialog()
+
+
+$imgPath = $FileBrowser.FileName
+Connect-PnPOnline -Url $tenantAdmin -UseWebLogin
+$HubSite = Get-PnPHubSite $hubSiteurl
+$HubSiteId = $HubSite.SiteId
+$ModernSites = (Get-PnPTenantSite -Template 'GROUP#0') + (Get-PnPTenantSite -Template 'SITEPAGEPUBLISHING#0') 
+$SitesFromHub = New-Object System.Collections.ArrayList
+Write-Host ("Searching {0} sites:" -f $HubSite.Title) -BackgroundColor Gray
+foreach ($ModernSite in $ModernSites) {
+    $site = Get-PnPHubSite $ModernSite.Url
+    if ($site.SiteUrl) {			
+        if ($site.SiteId -eq $HubSiteId) {
+            Write-Host ("* {0} - {1}" -f $ModernSite.Title, $ModernSite.Url)
+            $SitesFromHub.Add($ModernSite) | Out-Null
+        }
+    }
+}
+Write-Host ""
+Write-Host "Upload Logo at:" -BackgroundColor Gray
+foreach ($SiteHub in $SitesFromHub) {
+    Write-Host ("* {0} - {1} ... " -f $SiteHub.Title, $SiteHub.Url) -NoNewline	
+    Connect-PnPOnline -Url $SiteHub.Url -UseWebLogin
+    try {
+        New-PnPList -Title "Site Assets" -Template DocumentLibrary  -Url "SiteAssets" -ErrorAction SilentlyContinue
+        Add-PnPFile -Path $imgPath -Folder SiteAssets -ErrorAction Stop
+        $imgName = $imgPath | Split-Path -Leaf
+        $pathImg = (Get-PnPListItem -List SiteAssets -Fields FileRef).FieldValues | Where-Object { $_.FileRef -match $imgName }  
+        Set-PnPWeb -SiteLogoUrl $pathImg.FileRef		
+        Write-Host "Done" -BackgroundColor Green
+    }
+    catch {
+        Write-Host $_.ToString() -BackgroundColor Red
+    }
+}
+Write-Host "All Done"
