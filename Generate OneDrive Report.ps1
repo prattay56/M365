6c935746e57cf6635a5f2ad410bde9275228d023
Prattay D <74849664+prattay56@users.noreply.github.com>
2022-10-25 22:00:59 +0530

    Added Usage and allocated data

--- Generate OneDrive Report.ps1
+++ Generate OneDrive Report.ps1
@@ -1,7 +1,9 @@
 #variables
 $SiteAdminUPN = Read-Host "Enter Global Admin UPN"
 $TenantSiteURL = Read-Host "Enter SharePoint admin URL"
-$ReportOutput = Read-Host "Enter  report path EG C:\Temp\OneDriveReport.csv"
+$ReportOutput = Read-Host "Enter  report path Eg: C:\Temp\OneDriveReport.csv"
+
+Import-Module -Name pnp.powershell -Verbose
 
 function SetSiteAdmin {
     param (
@@ -49,11 +51,15 @@ Foreach ($Sites in $MySites) {
 
     Write-Host "Processing Site Collection :"$Sites.URL -f Yellow
     $CreatedDate = GetSiteCreationTime -VarSite $Sites.URL
+    $ConsumedSize = ($Sites.StorageUsageCurrent.ToString() + "MB")/1GB
+    $AllocatedSize = ($Sites.StorageQuota.ToString() + "MB")/1GB
 
     #Get site collection details   
     $Result = new-object PSObject
     $Result | add-member -membertype NoteProperty -name "Title" -Value $Sites.Title
     $Result | add-member -membertype NoteProperty -name "Url" -Value $Sites.Url
+    $Result | add-member -membertype NoteProperty -name "Usage(GB)" -Value $ConsumedSize
+    $Result | add-member -membertype NoteProperty -name "Allocated(GB)" -Value $AllocatedSize
     $Result | add-member -membertype NoteProperty -name "CreatedDate" -Value ($CreatedDate)
     $ResultSet += $Result
 }
