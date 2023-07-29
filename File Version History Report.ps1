789fda23170877e2375181a479ccb830144dbaac
Prattay D <74849664+prattay56@users.noreply.github.com>
2023-07-29 14:53:03 +0530

    Update File Version History Report.ps1

--- File Version History Report.ps1
+++ File Version History Report.ps1
@@ -1,30 +1,39 @@
-function Get-LibraryVersionReport {
-    param (
-        # Parameter help description
-        [Parameter(Mandatory = $true)]
-        [Microsoft.SharePoint.Client.List]
-        $Library,
-        # Parameter help description
-        [Parameter(Mandatory = $true)]
-        [string]
-        $CSVPath
-    )
-
-    # $Library = Get-PnPList -Identity $LibraryName
+#Parameters
+$SiteURL = "https://Domain.sharepoint.com/sites/AsiaSales"
+$CSVFile = "C:\Temp\VersionHistoryRpt.csv"
+$X = 8   #this is the number of versions to pull( here last 8 versions)
+ 
+#Delete the Output report file if exists
+If (Test-Path $CSVFile) { Remove-Item $CSVFile }
+ 
+#Connect to SharePoint Online site
+Connect-PnPOnline -Url $SiteURL -Interactive
+ 
+#Get All Document Libraries from the Web - Exclude Hidden and certain lists
+$ExcludedLists = @("Form Templates", "Preservation Hold Library", "Site Assets", "Pages", "Site Pages", "Images",
+    "Site Collection Documents", "Site Collection Images", "Style Library")
+$Lists = Get-PnPList | Where-Object { $_.Hidden -eq $False -and $_.Title -notin $ExcludedLists -and $_.BaseType -eq "DocumentLibrary" }
+ 
+#Iterate through all files from all document libraries
+ForEach ($List in $Lists) {
+    $global:counter = 0
+    $Files = Get-PnPListItem -List $List -PageSize 2000 -Fields File_x0020_Size, FileRef -ScriptBlock { Param($items) $global:counter += $items.Count; Write-Progress -PercentComplete ($global:Counter / ($List.ItemCount) * 100) -Activity "Getting Files of '$($List.Title)'" -Status "Processing Files $global:Counter to $($List.ItemCount)"; }  | Where { $_.FileSystemObjectType -eq "File" }
+     
     $VersionHistoryData = @()
-    #Iterate through all files
-    Get-PnPListItem -List $Library -PageSize 500 | Where { $_.FieldValues.FileLeafRef -like "*.*" } | ForEach-Object {
+    $Files | ForEach-Object {
         Write-host "Getting Versioning Data of the File:"$_.FieldValues.FileRef
-        #Get FileSize & version Size
+        #Get File Size and version Size
         $FileSizeinKB = [Math]::Round(($_.FieldValues.File_x0020_Size / 1KB), 2)
         $File = Get-PnPProperty -ClientObject $_ -Property File
-        $Versions = Get-PnPProperty -ClientObject $File -Property Versions
+        $AllVersions = Get-PnPProperty -ClientObject $File -Property Versions 
+        $Versions = $AllVersions | Sort-Object -Property Created -Descending | Select-Object -First $X
         $VersionSize = $Versions | Measure-Object -Property Size -Sum | Select-Object -expand Sum
         $VersionSizeinKB = [Math]::Round(($VersionSize / 1KB), 2)
         $TotalFileSizeKB = [Math]::Round(($FileSizeinKB + $VersionSizeinKB), 2)
- 
+  
         #Extract Version History data
         $VersionHistoryData += New-Object PSObject -Property  ([Ordered]@{
+                "Library Name"         = $List.Title
                 "File Name"            = $_.FieldValues.FileLeafRef
                 "File URL"             = $_.FieldValues.FileRef
                 "Versions"             = $Versions.Count
@@ -33,22 +42,5 @@ function Get-LibraryVersionReport {
                 "Total File Size (KB)" = $TotalFileSizeKB
             })
     }
-
-    $VersionHistoryData | Export-Csv -Path $CSVPath -NoTypeInformation -Append
-}
-
-#Set Variables
-$SiteURL = "https://DOMAIN.sharepoint.com/sites/XDR"
-$CSVPath = "C:\Temp\VersionHistoryRpt.csv"
-
-# connect to the desired site
-Connect-PnPOnline -Url $SiteURL -useweblogin
-
-# List only the document libraries in the site
-$AllLibraries = Get-PnPList | where { $_.BaseTemplate -eq 101 -and $_.Title -notin ("Form Templates", "Site Assets", "Style Library") }
-
-#Pull report for all the doc libs
-foreach($libs in $AllLibraries) {
-Write-Host "Generating report for Lib: $($libs.title)"
-    Get-LibraryVersionReport -Library $libs -CSVPath  $CSVPath
+    $VersionHistoryData | Export-Csv -Path $CSVFile -NoTypeInformation -Append
 }
